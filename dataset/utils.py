d47f8ea394079e856b8e4e58c4bcfc21a751d58a
Nilay Pande <nilay.pande@thematroid.com>
2022-08-03 18:00:17 -0700

    RSTPReid eval (WIP)

--- dataset/utils.py
+++ dataset/utils.py
@@ -40,6 +40,31 @@ def pre_caption(caption,max_words):
             
     return caption
 
+def pre_caption_custom(caption, max_words):
+    caption = re.sub(
+        r"([,.'!?\"()*#:;~])",
+        ' ',
+        caption.lower(),
+    ).replace('-', ' ').replace('/', ' ').replace('<person>', 'person')
+
+    caption = re.sub(
+        r"\s{2,}",
+        ' ',
+        caption,
+    )
+    caption = caption.rstrip('\n')
+    caption = caption.strip(' ')
+
+    # truncate caption
+    caption_words = caption.split(' ')
+    if len(caption_words) > max_words:
+        caption = ' '.join(caption_words[:max_words])
+
+    if not len(caption):
+        raise ValueError("pre_caption yields invalid text")
+
+    return caption
+
 
 from vqaTools.vqaEval import VQAEval
 from refTools.evaluation.refEvaluation import RefEvaluation
